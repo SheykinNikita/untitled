36b200519ee926579f179bea3b3393f1b0332828
SheykinNikita <43067432+SheykinNikita@users.noreply.github.com>
2018-09-07 16:06:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl; //я люблю Машу
     return 0;
 }
